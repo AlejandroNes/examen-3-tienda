55bcbb695411e74ece8bafb88a6725fdfb3f20eb
Alejandro Nes <alejandro315595@gmail.com>
2026-04-14 21:03:52 -0400

    agregando la seccion de proveedores

--- src/app/proveedores/proveedores.ts
+++ src/app/proveedores/proveedores.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TiendaService } from '../tienda';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-proveedores',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  // OJO AQUÍ: Si tu archivo HTML de esta carpeta se llama proveedores.component.html, agrégale el .component
   templateUrl: './proveedores.html',
-  styleUrl: './proveedores.css',
 })
-export class Proveedores {}
+export class ProveedoresComponent {
+  servicio = inject(TiendaService);
+
+  // Traemos el Signal de proveedores
+  misProveedores = this.servicio.listaProveedores;
+
+  // Variables del formulario
+  nombreNuevo = '';
+  telefonoNuevo = '';
+  idEditando: number | null = null; // Para saber si estamos creando o editando
+
+  guardar() {
+    // Verificamos que no envíen campos vacíos
+    if (this.nombreNuevo.trim() !== '' && this.telefonoNuevo.trim() !== '') {
+      const datos = { nombre: this.nombreNuevo, telefono: this.telefonoNuevo };
+
+      if (this.idEditando) {
+        this.servicio.actualizarProveedor(this.idEditando, datos); // EDITAR
+      } else {
+        this.servicio.agregarProveedor(datos); // CREAR
+      }
+      this.limpiar();
+    }
+  }
+
+  // Cuando le dan clic al botón "Editar" de la tabla
+  prepararEdicion(prov: any) {
+    this.idEditando = prov.id;
+    this.nombreNuevo = prov.nombre;
+    this.telefonoNuevo = prov.telefono;
+  }
+
+  eliminar(id: number | undefined) {
+    if (id) this.servicio.borrarProveedor(id);
+  }
+
+  limpiar() {
+    this.idEditando = null;
+    this.nombreNuevo = '';
+    this.telefonoNuevo = '';
+  }
+}
